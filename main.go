851727e0da693c072dc6a4d36d10b977c5dd8ed3
sourabh <sourabhb@buffalo.edu>
2018-03-19 14:00:41 -0400

    added all fiels in single main file to run n CCR;

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
-	"github.com/PPSO/plot"
+	"github.com/Arafatk/glot"
+	"github.com/PPSO/constants"
+	"github.com/satori/go.uuid"
 )
 
 
@@ -184,8 +186,57 @@ func main() {
 	//fmt.Println("plotYPoints = ", plotYPoints)
 	//fmt.Println("plotXPoints = ", plotXPoints)
 	//plot graph
-	plot.PlotGraph(plotXPoints,plotYPoints)
+	PlotGraph(plotXPoints,plotYPoints)
 
 	log.Info("Graph Plot Done")
 }
+
+
+
+//plotGraph - plots the 2D Graph for the points
+func PlotGraph(xPoints []float64, yPoints []float64) {
+	dimensions := 2
+	// The dimensions supported by the plot
+	persist := false
+	debug := false
+	plot, _ := glot.NewPlot(dimensions, persist, debug)
+	pointGroupName := constants.FitnessValue
+	style := "lines"
+	points := [][]float64{xPoints, yPoints}
+	// Adding a point group
+	plot.AddPointGroup(pointGroupName, style, points)
+	// A plot type used to make points/ curves and customize and save them as an image.
+	plot.SetTitle(constants.PSOPlotTitle)
+	// Optional: Setting the title of the plot
+	plot.SetXLabel(constants.XAxisLabel)
+	plot.SetYLabel(constants.YAxisLabel)
+	// Optional: Setting label for X and Y axis
+
+
+	_,maxX := MinMax(xPoints)
+	_,maxY := MinMax(yPoints)
+
+	plot.SetXrange(0, int(maxX))
+	plot.SetYrange(0, int(maxY) * 2)
+
+	uuidN,_ := uuid.NewV1()
+	destinationFile := constants.GraphFolderName + constants.Slash + constants.PPSO + uuidN.String() + constants.DotPNG
+	plot.SavePlot(destinationFile)
+}
+
+
+func MinMax(array []float64) (float64, float64) {
+	var max float64 = array[0]
+	var min float64 = array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+		if min > value {
+			min = value
+		}
+	}
+	return min, max
+}
+
 
